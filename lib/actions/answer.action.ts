b54747fae0d4044564ec7d639204dc0d860958a1
Bartosz  Podemski <bartekpodemski@gmail.com>
2023-11-26 11:29:41 +0100

    Add question detail page

---
+++ lib/actions/answer.action.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import Answer from "@/database/answer.model";
+import { connectToDatabase } from "../mongoose";
+import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
+
+export async function createAnswer(params: CreateAnswerParams) {
+  try {
+    connectToDatabase();
+
+    const { content, author, question, path } = params;
+
+    const answer = await Answer.create({
+      content,
+      author,
+      question,
+    });
+
+    await Question.findByIdAndUpdate(
+      question,
+      { $push: { answers: answer._id } },
+      { new: true }
+    );
+
+    // TODO: Add interaction
+
+    revalidatePath(path);
+    return { answer };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAnswers(params: GetAnswersParams) {
+  try {
+    connectToDatabase();
+
+    const { questionId } = params;
+
+    const answers = await Answer.find({ question: questionId })
+      .populate({
+        path: "author",
+        model: "User",
+        select: "_id clerkId name picture",
+      })
+      .sort({ createdAt: -1 });
+
+    return { answers };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
